2a326f4ae5651d5b205376c63a9b0b8a6b6fe199
Rajendra Kumar Bhochalya <raj@rkb.io>
2019-02-03 20:54:13 +0530

    Initial Commit

---
+++ GrayscaleMode/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GrayscaleMode
+//
+//  Created by Rajendra Bhochalya on 03/02/19.
+//  Copyright © 2019 Rajendra Bhochalya. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GrayscaleMode/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GrayscaleMode
+//
+//  Created by Rajendra Bhochalya on 03/02/19.
+//  Copyright © 2019 Rajendra Bhochalya. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
